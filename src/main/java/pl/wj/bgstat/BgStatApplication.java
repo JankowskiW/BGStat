b4c9252e5aaf9494cd877a6834aaa3fc8c871a1c
jankowskiw <waldemar.jankowski95@gmail.com>
2022-03-11 21:10:34 +0100

    Project structure
    Utworzono początkową strukturę projektu oraz zaimportowano niezbędne biblioteki

---
+++ src/main/java/pl/wj/bgstat/BgStatApplication.java
@@ -0,0 +1,13 @@
+package pl.wj.bgstat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgStatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BgStatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/wj/bgstat/BgStatApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wj.bgstat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgStatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
