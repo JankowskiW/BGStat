94e2098e61e1419e09b2945b84864b5d82e5fc19
jankowskiw <waldemar.jankowski95@gmail.com>
2022-03-30 18:05:22 +0200

    Added BoardGameDescriptionServiceTest class

---
+++ src/test/java/pl/wj/bgstat/boardgamedescription/BoardGameDescriptionServiceTest.java
@@ -0,0 +1,11 @@
+package pl.wj.bgstat.boardgamedescription;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class BoardGameDescriptionServiceTest {
+
+}
