5163c5f9065e3dcaf00a60b4041d6e8e16874f79
jankowskiw <waldemar.jankowski95@gmail.com>
2022-05-12 18:58:27 +0200

    Create UserBoardgameController methods

--- src/main/java/pl/wj/bgstat/userboardgame/UserBoardGameController.java
+++ src/main/java/pl/wj/bgstat/userboardgame/UserBoardGameController.java
@@ -1,8 +1,13 @@
 package pl.wj.bgstat.userboardgame;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pl.wj.bgstat.userboardgame.model.UserBoardGame;
+import pl.wj.bgstat.userboardgame.model.dto.UserBoardGameDetailsDto;
+import pl.wj.bgstat.userboardgame.model.dto.UserBoardGameRequestDto;
+import pl.wj.bgstat.userboardgame.model.dto.UserBoardGameResponseDto;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -10,4 +15,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserBoardGameController {
 
     private final UserBoardGameService userBoardGameService;
+
+    @GetMapping("/{id}")
+    public UserBoardGameDetailsDto getSingleUserBoardGame(@PathVariable long id) {
+        return userBoardGameService.getSingleUserBoardGame(id);
+    }
+
+    @PostMapping("")
+    public UserBoardGameResponseDto addUserBoardGame(@RequestBody @Valid UserBoardGameRequestDto userBoardGameRequestDto) {
+        return userBoardGameService.addUserBoardGame(userBoardGameRequestDto);
+    }
+
+    @PutMapping("/{id}")
+    public UserBoardGameResponseDto editUserBoardGame(
+            @PathVariable long id, UserBoardGameRequestDto userBoardGameRequestDto) {
+        return userBoardGameService.editUserBoardGame(id, userBoardGameRequestDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUserBoardGame(@PathVariable long id) {
+        userBoardGameService.deleteUserBoardGame(id);
+    }
 }
