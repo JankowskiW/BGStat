d22ccb5bc1ea423dfb8df878ca52b97396cbb312
jankowskiw <waldemar.jankowski95@gmail.com>
2022-05-24 20:23:35 +0200

    create add, edit and delete gameplay controller methods

--- src/main/java/pl/wj/bgstat/gameplay/GameplayController.java
+++ src/main/java/pl/wj/bgstat/gameplay/GameplayController.java
@@ -2,12 +2,12 @@ package pl.wj.bgstat.gameplay;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pl.wj.bgstat.gameplay.model.dto.GameplayRequestDto;
+import pl.wj.bgstat.gameplay.model.dto.GameplayResponseDto;
 import pl.wj.bgstat.gameplay.model.dto.GameplaysStatsDto;
 
+import javax.validation.Valid;
 import java.time.LocalDate;
 
 @RestController
@@ -23,4 +23,17 @@ public class GameplayController {
             @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate toDate) {
         return gameplayService.getGameplayStats(fromDate, toDate);
     }
+
+    @PostMapping("")
+    public GameplayResponseDto addGameplay(@RequestBody @Valid GameplayRequestDto gameplayRequestDto) {
+        return gameplayService.addGameplay(gameplayRequestDto);
+    }
+
+    @PutMapping("/{id}")
+    public GameplayResponseDto editGameplay(@PathVariable long id, @RequestBody GameplayRequestDto gameplayRequestDto) {
+        return gameplayService.editGameplay(id, gameplayRequestDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteGameplay(@PathVariable long id) {gameplayService.deleteGameplay(id);}
 }
