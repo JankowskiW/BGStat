53cbdc5e80e72cb995aec94999e4afbb46eb4d1b
jankowskiw <waldemar.jankowski95@gmail.com>
2022-06-24 23:11:21 +0200

    Add validation messages to AttributeRequestDto

--- src/main/java/pl/wj/bgstat/attribute/model/dto/AttributeRequestDto.java
+++ src/main/java/pl/wj/bgstat/attribute/model/dto/AttributeRequestDto.java
@@ -4,15 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class AttributeRequestDto {
+    @Min(value = 1, message = "Attribute object type id should be positive integer number")
     private long objectTypeId;
+    @Min(value = 1, message = "Attribute object type id should be positive integer number")
     private long objectId;
+    @Min(value = 1, message = "Attribute object type id should be positive integer number")
     private long attributeClassId;
+    @NotBlank(message = "Attribute value cannot be blank")
+    @Length(max = 255, message = "Attribute value cannot be longer than 255 characters")
     private String value;
     private boolean multivaluedAttributeClassType;
 }
